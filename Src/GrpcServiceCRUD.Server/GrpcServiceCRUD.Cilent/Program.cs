e46bad26dc23483de14b0e1f1ab14b9bdff95a98
Paulwu <paulwu@gis.tw>
2022-05-03 18:40:39 +0800

    新增 稍微紀錄與嘗試一下 gRPC 的使用方法與開發

---
+++ Src/GrpcServiceCRUD.Server/GrpcServiceCRUD.Cilent/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServiceCRUD;
+using GrpcServiceCRUD.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5258");
+var client = new Greeter.GreeterClient(channel);
+
+var helloReply =client.SayHello(new HelloRequest()
+{
+    Name = "Mobe Wu"
+});
+
+
+var client2 = new Algorithmer.AlgorithmerClient(channel);
+
+var sum = await client2.AlgorithmAdditionAsync(new AddTwoValue { Addend = 10 , Augend = 10 });
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(helloReply.Message);
+Console.WriteLine(sum.Sum);
+Console.ReadLine();
---
+++ Src/GrpcServiceCRUD.Server/GrpcServiceCRUD/Services/AlgorithmerService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using GrpcServiceCRUD.Protos;
+
+namespace GrpcServiceCRUD.Services
+{
+    public class AlgorithmerService : Algorithmer.AlgorithmerBase
+    {
+        private readonly ILogger<AlgorithmerService> _logger;
+        public AlgorithmerService(ILogger<AlgorithmerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<SumTwoValue> AlgorithmAddition(AddTwoValue request, ServerCallContext context)
+        {
+            return Task.FromResult(new SumTwoValue
+            {
+                Sum = request.Addend + request.Augend
+            });
+        }
+    }
+}
